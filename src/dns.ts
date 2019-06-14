0bf50c6d69b579cc4da0e1c60cced9af5e517ac3
William Desportes <williamdes@wdes.fr>
2019-06-14 14:35:12 +0200

    added: CLOUDFLARE_RECORD_TYPE to ENV

--- src/dns.ts
+++ src/dns.ts
@@ -8,6 +8,35 @@ const cf = require('cloudflare')({
 
 import data from '@src/data';
 
+export interface CFRecord {
+    content: string;
+    name: string;
+    priority?: number;
+    proxied?: boolean;
+    ttl?: 1 | 120 | 300 | 600 | 900 | 1800 | 3600 | 7200 | 18000 | 43200 | 86400;
+    type:
+        | 'A'
+        | 'AAAA'
+        | 'CNAME'
+        | 'CAA'
+        | 'CERT'
+        | 'DNSKEY'
+        | 'DS'
+        | 'LOC'
+        | 'MX'
+        | 'NAPTR'
+        | 'NS'
+        | 'PTR'
+        | 'SRV'
+        | 'SPF'
+        | 'TXT'
+        | 'SMIMEA'
+        | 'SSHFP'
+        | 'TLSA'
+        | 'URI'
+        | string;
+}
+
 export default {
     publishDnsRecord: (containerName: string, prId: number, ref: string, sha: string) => {
         return new Promise((resolve: (domainName: string) => void, reject: (err: Error | null) => void) => {
@@ -20,18 +49,20 @@ export default {
                 },
                 process.env.CLOUDFLARE_RECORD_NAME || ''
             );
+            const record: CFRecord = {
+                type: process.env.CLOUDFLARE_RECORD_TYPE || 'A',
+                name: domainName,
+                content: process.env.CLOUDFLARE_RECORD_CONTENT || '127.0.0.53',
+                proxied: true,
+            };
+
             cf.dnsRecords
-                .add(process.env.CLOUDFLARE_ZONEID, {
-                    type: 'A',
-                    name: domainName,
-                    content: process.env.CLOUDFLARE_RECORD_CONTENT,
-                    proxied: true,
-                })
-                .then(function() {
+                .add(process.env.CLOUDFLARE_ZONEID, record)
+                .then(() => {
                     logger.info('Added:', domainName);
                     resolve(domainName);
                 })
-                .catch(function() {
+                .catch(() => {
                     logger.info('Not added:', domainName);
                     resolve(domainName);
                 });
