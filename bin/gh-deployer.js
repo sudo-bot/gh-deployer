ce54b1a9c1f4929eaca0e5eee5db46b2b21b8c1b
William Desportes <williamdes@wdes.fr>
2021-02-12 19:26:34 +0100

    fix: sentry appender and module loading

--- bin/gh-deployer.js
+++ bin/gh-deployer.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 const moduleAlias = require('module-alias');
 moduleAlias.addAliases({
-    '@src': __dirname + '/../dist',
+    '@src': __dirname + '/../dist/src',
+    '@utils': __dirname + '/../dist/utils',
+    '@root': __dirname + '/../dist',
 });
 
 moduleAlias();
-
 require('@sudo-bot/gh-deployer');
